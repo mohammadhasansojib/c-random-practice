2cdbe1ef914e1909ccf48174083cfa01dac155f4
sajib hasan <sajibhasan800080@gmail.com>
2023-11-12 21:06:59 +0600

    created c file and added structure and code

--- random-practice.c
+++ random-practice.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world");
+
+    int myarray[] = {4,6,2,7,3};
+    int int_size;
+
+    int myarray_length = sizeof(myarray) / sizeof(int_size);
+
+//    for(int i = 0; i < 10; i++){
+//        printf("%d ", myarray[i]);
+//    }
+
+
 
     return 0;
 }
