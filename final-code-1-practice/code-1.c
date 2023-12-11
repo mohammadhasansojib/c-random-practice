6e1c9bdfc6b6064437d577b18fc89a80da0b8b7e
sajib hasan <sajibhasan800080@gmail.com>
2023-12-11 23:58:05 +0600

    added code in code-1 file of 'final code-1' folder

--- final-code-1-practice/code-1.c
+++ final-code-1-practice/code-1.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
 int main(){
+    int len;
+    printf("enter the length of the array: ");
+    scanf("%d", &len);
+
+    int arr1[len], arr2[len];
+
+    for(int i = 0; i < len; i++){
+        printf("arr[%d] : ", i);
+        scanf("%d", &arr1[i]);
+        arr2[i] = arr1[i];
+    }
+
+
+
+
+    int ic = 0;//index controler
+
+    for(int i = 0; i < len; i++){
+        if((arr1[i]%2) == 0){
+            arr1[ic] = arr1[i];
+            ic++;
+        }
+    }
+
+
+    for(int i = 0; i < len; i++){
+        if((arr2[i]%2) != 0){
+            arr1[ic] = arr2[i];
+            ic++;
+        }
+    }
+
+
+
+    printf("\nafter separation: ");
+    for(int i = 0; i < len; i++){
+        printf("%d ", arr1[i]);
+    }
 
 
 
