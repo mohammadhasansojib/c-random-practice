8b662caeff776a4646276eb4c404ed305b891a7c
sajib hasan <sajibhasan800080@gmail.com>
2023-12-12 11:38:17 +0600

    added code in code-2.c file of 'final-code-2-practice' folder

--- final-code-2-practice/code-2.c
+++ final-code-2-practice/code-2.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
+void rm_space(char *str) {
+    int i, j;
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            str[j++] = str[i];
+        }
+    }
 
-int main(){
+    str[j] = '\0';
+}
 
+int main() {
+    char str[100];
 
+    printf("Enter a string: ");
+    gets(str);
+
+    rm_space(str);
+
+    printf("String after removing white spaces: %s\n", str);
 
     return 0;
 }
