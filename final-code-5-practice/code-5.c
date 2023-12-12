b69aa334a178fa80bbf0edfe69598546c0014c76
sajib hasan <sajibhasan800080@gmail.com>
2023-12-13 00:54:03 +0600

    added code in code-5.c file of 'final-code-5-practice' folder

--- final-code-5-practice/code-5.c
+++ final-code-5-practice/code-5.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
+int fact(int num){
+
+    int fact = 1;
+    for(int i = num; i > 0; i--){
+        fact *= i;
+    }
+
+    return fact;
+}
+
+
+int series(int num){
+
+    int series_sum = 0;
+
+    for(int i = 1; i <= num; i++){
+        printf(" + %d!/%d", i,i);
+        series_sum += fact(i) / i;
+    }
+
+    printf(" = %d", series_sum);
+}
+
 
 int main(){
 
+    int num;
+    printf("enter n: ");
+    scanf("%d", &num);
 
+    series(num);
 
     return 0;
 }
