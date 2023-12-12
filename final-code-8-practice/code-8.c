47bb0fde469ccd62040cbdbbcf5a54016c125acd
sajib hasan <sajibhasan800080@gmail.com>
2023-12-13 02:00:16 +0600

    added code in code-8.c of 'final-code-8-practice' folder

--- final-code-8-practice/code-8.c
+++ final-code-8-practice/code-8.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
+int is_prime(int num){
+    int boolean = 1;
+
+    for(int i = 2; i < num; i++){
+        if(num%i == 0){
+            boolean = 0;
+            break;
+        }
+    }
+
+    return boolean;
+}
+
+
 int main(){
 
+    int s,e, sum = 0;
+    scanf("%d%d", &s,&e);
 
+
+    for(int i = s; i <= e; i++){
+        if(is_prime(i)){
+            printf("%d ", i);
+            sum += i;
+        }
+    }
+
+    printf("\nsum = %d", sum);
 
     return 0;
 }
