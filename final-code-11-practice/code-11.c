73446a37f4c61a313803b01c643e60c4daa1f301
sajib hasan <sajibhasan800080@gmail.com>
2023-12-13 12:30:05 +0600

    added code in code-11.c of 'final-code-11-practice' folder

--- final-code-11-practice/code-11.c
+++ final-code-11-practice/code-11.c
@@ -1,7 +1,60 @@
+//#include <stdio.h>
+//
+//int main(){
+//
+//    char str[100], c, maxchar;
+//    printf("enter the string: ");
+//    gets(str);
+//
+//    int max = 0, counter;
+//
+//    for(c = 'a'; c <= 'z'; c++){
+//        counter = 0;
+//        for(int i = 0; str[i] != '\0'; i++){
+//            if(c==str[i]){
+//                counter++;
+//                if(counter > max){
+//                    max = counter;
+//                    maxchar = c;
+//                }
+//            }
+//        }
+//    }
+//    printf("\n%c has maximum times(%d times)", maxchar,max);
+//
+//
+//    return 0;
+//}
+
+
+
+
+
+
 #include <stdio.h>
 
 int main(){
 
+    char str[100], maxchar, c;
+    gets(str);
+
+    int counter, max;
+
+    for(c = 'a'; c <= 'z'; c++){
+        counter = 0;
+        for(int i = 0; str[i] != '\0'; i++){
+            if(c==str[i]){
+                counter++;
+                if(counter > max){
+                    max = counter;
+                    maxchar = c;
+                }
+            }
+        }
+    }
+    printf("\n%c has maximum times(%d times)", maxchar,max);
 
     return 0;
 }
+
+
