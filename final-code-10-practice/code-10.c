004223ee88e31378c59fed4afa883aa78e88fae1
sajib hasan <sajibhasan800080@gmail.com>
2023-12-14 13:50:15 +0600

    added code in code-10.c in 'final-code-10-practice' folder

--- final-code-10-practice/code-10.c
+++ final-code-10-practice/code-10.c
@@ -2,6 +2,25 @@
 
 int main(){
 
+    char str1[100], str2[100];
+    printf("enter a string: ");
+    gets(str1);
+
+    char *p1,*p2;
+
+    p1 = str1 + strlen(str1)-1;
+    p2 = str2;
+
+    while(p1 >= str1){
+        *p2 = *p1;
+        p1--;
+        p2++;
+    }
+
+    *p2 = '\0';
+
+    printf("after reverse : %s", str2);
+
 
     return 0;
 }
