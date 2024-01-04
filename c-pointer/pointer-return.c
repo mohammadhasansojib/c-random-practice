6f316c64164b49b374fe53788fe29a435b25dcfb
sajib hasan <sajibhasan800080@gmail.com>
2024-01-04 18:36:39 +0600

    added code in pointer-return.c of 'c-pointer' folder

--- c-pointer/pointer-return.c
+++ c-pointer/pointer-return.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+int *mid(int i[], int len){
+    return &i[len/2];
+}
+
+int *hello(int a[]){
+    return &a[0];
+}
+
 int main(){
 
-    printf("Hello World");
+    int i[] = {1,2,3,4,5};
+    int len = sizeof(i) / sizeof(i[0]);
+
+    int *pr = mid(i,len);
+    printf("%d", *pr);
+
+    int a[] = {10,20,30,40,50};
+    int *pr2 = hello(a);
+    printf("\n%d", *pr2);
+
 
     return 0;
 }
