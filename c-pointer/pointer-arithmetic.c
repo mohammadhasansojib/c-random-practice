41eb9247d19f2995a3dd1ef908a386419a977bc7
sajib hasan <sajibhasan800080@gmail.com>
2024-01-05 23:47:40 +0600

    added more code about pointer substraction

--- c-pointer/pointer-arithmetic.c
+++ c-pointer/pointer-arithmetic.c
@@ -6,10 +6,44 @@ int main(){
     int len = sizeof(a) / sizeof(a[3]);
 
     int *p = a;
+    int *x = &a[2];
+    int *y = &a[5];
 
     for(int i = 0; i < len; i++){
-        printf("%d ", *(p+i));
+        printf("%d (%d)\n", *(p+i),(p+i));
     }
+    printf("\n");
+    for(int i = 0; i < len; i++){
+        printf("%d (%d)\n", *((p+len-1)-i),((p+len-1)-i));
+    }
+
+    printf("%d\n", *(y-2));
+
+
+
+
+    printf("\n");
+    int a2 = 40;
+    int a3 = 20;
+    int a4 = 70;
+
+    for(int i = 0; i < 12; i+=4){
+        printf("%d\n", *(&a2-i));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     char b[] = {'a','b','c','d','e'};
     int n = sizeof(b) / sizeof(b[4]);
