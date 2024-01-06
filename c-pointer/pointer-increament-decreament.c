d2a2cec35e83527da2dc0832f89e24f0ad9fe517
sajib hasan <sajibhasan800080@gmail.com>
2024-01-06 17:25:01 +0600

    added code in pointer-increament-decreament.c file of 'c-pointer' folder

--- c-pointer/pointer-increament-decreament.c
+++ c-pointer/pointer-increament-decreament.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+int myFun(int *x[]){
+
+    printf("\n%d", x);
+
+}
+
+
 int main(){
+
+    int x[] = {5,16,32,61,43};
+    int *p = x;
+
+    int len = sizeof(x) / sizeof(x[0]);
+
+    for(int i = 0; i < len; i++){
+        printf("%d ", *(p++));
+    }
+
+    printf("\n%d", p);
+
+    myFun(x);
+
 
     return 0;
 }
